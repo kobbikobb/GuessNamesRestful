3020eb72ced83563138daa3b47541e340916182a
Jakob <jakob@motus.is>
2014-11-22 21:51:46 +0000

    Statistics

---
+++ GuessNamesRestful/Controllers/StatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GuessNamesRestful.Models;
+
+namespace GuessNamesRestful.Controllers
+{
+    public class StatisticsController : ApiController
+    {
+        private readonly GuessNamesContext guessNamesContext;
+
+        public StatisticsController()
+        {
+            guessNamesContext = new GuessNamesContext();
+        }
+
+        public Statistics Get(int nameId)
+        {
+            var name = guessNamesContext.Names.Single(x => x.Id == nameId);
+
+            return new Statistics
+            {
+                TotalTries = name.Guesses.Count,
+                PartiallyCorrectTries = name.Guesses.Count(x => x.Score == Score.PartiallyCorrect),
+                CorrectTries = name.Guesses.Count(x => x.Score == Score.Correct)
+            };
+        }
+    }
+}
---
+++ GuessNamesRestful/Models/Statistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuessNamesRestful.Models
+{
+    public class Statistics
+    {
+        public int TotalTries { get; set; }
+        public int PartiallyCorrectTries { get; set; }
+        public int CorrectTries { get; set; }
+    }
+}
